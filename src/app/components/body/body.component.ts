80b8134aeaa7cac5453c4db5b73f6a9bf485584d
Daniel Ortiz <daniortiz98@hotmail.com>
2019-10-15 17:17:55 -0500

    Version con el funcionamiento de *ngIf y *ngFor

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./body.component.css']
 })
 export class BodyComponent implements OnInit {
+	
+	mostrar = true;
+	personajes: string[] = ['Batman','Venom','Spiderman','Dr. Octopus'];
+	frase: any = {
+		mensaje: 'Un gran poder requiere una gran responsabilidad', 
+		autor: 'Ben Parker'
+	};
 
   constructor() { }
 
